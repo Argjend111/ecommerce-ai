b9e00c89229613d064f121801f9aa63a5a086f6a
Argjend Bytyçi <argjendd220@gmail.com>
2025-12-14 23:57:28 +0100

    ch

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,15 @@
+'use client'
+
 import { ClerkProvider } from '@clerk/nextjs'
 import React from 'react'
+import { CartStoreProvider } from '@/libs/store/cart-store-povider'
+import { ChatStoreProvider } from '@/stores/chat-store'
+import { AppShell } from '@/components/app-shell'
+import { Header } from '@/components/header'
+import { CartSheet } from '@/components/cart-sheet'
+import { ChatSheet } from '@/components/chat-sheet'
+import { Toaster } from 'react-hot-toast'
+import { SanityLive } from '@/sanity/lib/live'
 
 function AppLayout({ children }: { children: React.ReactNode }) {
     return (
