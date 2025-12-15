128462c763a65f051c852bba620179d514035b47
Argjend Bytyçi <argjendd220@gmail.com>
2025-12-15 12:57:04 +0100

    ubdated schema type

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,8 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import { productType } from './product'
-import { categoryType } from './category'
-import { customerType } from './customer'
-import { orderType } from './order'
+import { productType } from './productType'
+import { categoryType } from './categoryType'
+import { customerType } from './customerType'
+import { orderType } from './orderType'
 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
