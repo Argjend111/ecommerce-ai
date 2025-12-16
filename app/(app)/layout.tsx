360693fe1b4cefeb5b33d78db1a4010451c53a63
Argjend Bytyçi <argjendd220@gmail.com>
2025-12-16 13:30:00 +0100

    changes

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,25 +1,30 @@
 import { CartStoreProvider } from "@/lib/store/cart-store-provider";
+import { ChatStoreProvider } from "@/lib/store/chat-store-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import { SanityLive } from "@/sanity/lib/live";
-import { Toaster } from "sonner";
 import { Header } from "@/components/app/Header";
-import { ChatStoreProvider } from "@/lib/store/chat-store-provider";
 import { CartSheet } from "@/components/app/CartSheet";
-
+import { AppShell } from "@/components/app/AppShell";
+import { Toaster } from "sonner";
+import { ChatSheet } from "@/components/app/ChatSheet";
 
 function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <CartStoreProvider>
         <ChatStoreProvider>
-          <Header />
-          <main>{children}</main>
-          <CartSheet/>
-          <Toaster position="bottom-center"/>
+          <AppShell>
+            <Header />
+            <main>{children}</main>
+          </AppShell>
+          <CartSheet />
+          <ChatSheet />
+          <Toaster position="bottom-center" />
           <SanityLive />
         </ChatStoreProvider>
       </CartStoreProvider>
     </ClerkProvider>
   );
 }
+
 export default AppLayout;
--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,7 +1,7 @@
 
 import { defineLive } from "next-sanity/live";
-import { client } from './client'
+import { client } from "./client";
 
-export const { sanityFetch, SanityLive } = defineLive({
+export const { sanityFetch, SanityLive, SanityApp } = defineLive({
   client,
 });
