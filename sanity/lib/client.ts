aadc20e645b34404384a4c6e3c62e6956ab3d3af
Argjend Bytyçi <argjendd220@gmail.com>
2025-12-16 13:45:19 +0100

    ch

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,19 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId } from "../env";
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true,  
-})
+  useCdn: false, 
+  perspective: "published",
+});
+
+export const writeClient = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: false,
+  token: process.env.SANITY_API_WRITE_TOKEN,
+});
